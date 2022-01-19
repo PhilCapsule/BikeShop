903d537bdebfa7f0ec6fddf68c189e345abb9fb3
Phil <phildev013@gmail.com>
2022-01-19 11:34:02 +0100

    correctif icon

--- bikeshop/routes/index.js
+++ bikeshop/routes/index.js
@@ -1,34 +1,27 @@
-const { Router } = require('express');
 var express = require('express');
 var router = express.Router();
 
-
-// FakeDB
 var dataBike = [
-  {name:"Shimano", url:"/images/bike-1.jpg", price:679},
-  {name:"Peugeot", url:"/images/bike-2.jpg", price:800},
-  {name:"Mongoose", url:"/images/bike-3.jpg", price:1100},
-  {name:"Campagnolo", url:"/images/bike-4.jpg", price:899},
-  {name:"Mercier", url:"/images/bike-5.jpg", price:2599},
-  {name:"Vario", url:"/images/bike-6.jpg", price:2100}
+  {name:"BIK045", url:"/images/bike-1.jpg", price:679},
+  {name:"ZOOK07", url:"/images/bike-2.jpg", price:999},
+  {name:"TITANS", url:"/images/bike-3.jpg", price:799},
+  {name:"CEWO", url:"/images/bike-4.jpg", price:1300},
+  {name:"AMIG039", url:"/images/bike-5.jpg", price:479},
+  {name:"LIK099", url:"/images/bike-6.jpg", price:869},
 ]
 
-
-//FakeDbBasket
 var dataCardBike = [
-  {name:"Shimano", url:"/images/bike-1.jpg", price:679, quantity:1},
-  {name:"Mongoose", url:"/images/bike-3.jpg", price:1100, quantity:1},
-  {name:"Vario", url:"/images/bike-6.jpg", price:2100, quantity:1}
+  {name:"BIK045", url:"/images/bike-1.jpg", price:679, quantity:1},
+  {name:"ZOOK07", url:"/images/bike-2.jpg", price:999, quantity:2},
 ]
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {dataBike:dataBike});
 });
 
-router.get('/shop', function(req, res, next){
-  res.render('shop', {dataCardBike:dataCardBike})
+router.get('/shop', function(req, res, next) {
+  res.render('shop', {dataCardBike:dataCardBike});
 });
 
 module.exports = router;

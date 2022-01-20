f383cb0f965535ae1c0a6307193e2b62814fbe85
Phil <phildev013@gmail.com>
2022-01-20 15:10:04 +0100

    stripe process

--- bikeshop/routes/index.js
+++ bikeshop/routes/index.js
@@ -1,36 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+const stripe = require('stripe')('sk_test_51JZxmGBD4dyiStu4FY73NIwK44gYpcLoIBZBS7lBqy6NgXTMkrUqr2Fx4VZ0tptDf4ZgazDCWQCpeHOdN0DUI70300D25SvNsI');
+
 var dataBike = [
-  {name:"BIK045", url:"/images/bike-1.jpg", price:679},
-  {name:"ZOOK07", url:"/images/bike-2.jpg", price:999},
-  {name:"TITANS", url:"/images/bike-3.jpg", price:799},
-  {name:"CEWO", url:"/images/bike-4.jpg", price:1300},
-  {name:"AMIG039", url:"/images/bike-5.jpg", price:479},
-  {name:"LIK099", url:"/images/bike-6.jpg", price:869},
+  { name: "BIK045", url: "/images/bike-1.jpg", price: 679 },
+  { name: "ZOOK07", url: "/images/bike-2.jpg", price: 999 },
+  { name: "TITANS", url: "/images/bike-3.jpg", price: 799 },
+  { name: "CEWO", url: "/images/bike-4.jpg", price: 1300 },
+  { name: "AMIG039", url: "/images/bike-5.jpg", price: 479 },
+  { name: "LIK099", url: "/images/bike-6.jpg", price: 869 },
 ]
 
 
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
-  if(req.session.dataCardBike == undefined){
-    req.session.dataCardBike = []
+  if (req.session.dataCardBike == undefined) {
+    req.session.dataCardBike = [];
   }
-  res.render('index', {dataBike:dataBike});
-});
- 
 
-// Shop
-router.get('/shop', function(req, res, next) {
+  res.render('index', { dataBike: dataBike });
+});
+
+router.get('/shop', function (req, res, next) {
+
+  if (req.session.dataCardBike == undefined) {
+    req.session.dataCardBike = [];
+  }
 
   var alreadyExist = false;
-
-  for(var i = 0; req.session.dataCardBike.length; i++){
-    if(req.session.dataCardBike[i].name == req.query.bikeNameFromFront){
-    req.session.dataCardBike[i].quantity = req.session.dataCardBike[i].quantity +1;
-    alreadyExist = true;
+  for (var i = 0; i < req.session.dataCardBike.length; i++) {
+    if (req.session.dataCardBike[i].name == req.query.bikeNameFromFront) {
+      req.session.dataCardBike[i].quantity = Number(req.session.dataCardBike[i].quantity) + 1;
+      alreadyExist = true;
     }
   }
 
@@ -43,30 +46,68 @@ router.get('/shop', function(req, res, next) {
     })
   }
 
-
-
-  res.render('shop', {dataCardBike:req.session.dataCardBike});
+  res.render('shop', { dataCardBike: req.session.dataCardBike });
 });
 
+router.get('/delete-shop', function (req, res, next) {
 
-//shop
-router.get('/delete-shop', function(req, res, next){
-  
-  req.session.dataCardBike.splice(req.query.position,1)
+  if (req.session.dataCardBike == undefined) {
+    req.session.dataCardBike = [];
+  }
 
-  res.render('shop',{dataCardBike:req.session.dataCardBike})
+  req.session.dataCardBike.splice(req.query.position, 1)
+
+  res.render('shop', { dataCardBike: req.session.dataCardBike })
 })
 
+router.post('/update-shop', function (req, res, next) {
 
+  if (req.session.dataCardBike == undefined) {
+    req.session.dataCardBike = [];
+  }
 
-router.post('/update-shop', function(req, res, next){
-  
   var position = req.body.position;
   var newQuantity = req.body.quantity;
 
-  dataCardBike[position].quantity = newQuantity;
+  req.session.dataCardBike[position].quantity = newQuantity;
 
-  res.render('shop',{dataCardBike:req.session.dataCardBike})
+  res.render('shop', { dataCardBike: req.session.dataCardBike })
+})
+
+router.post('/create-checkout-session', async (req, res) => {
+
+  if (req.session.dataCardBike == undefined) {
+    req.session.dataCardBike = [];
+  }
+
+  var stripeItems = [];
+
+  for (var i = 0; i < req.session.dataCardBike.length; i++) {
+    stripeItems.push({
+      price_data: {
+        currency: 'eur',
+        product_data: {
+          name: req.session.dataCardBike[i].name,
+        },
+        unit_amount: req.session.dataCardBike[i].price * 100,
+      },
+      quantity: req.session.dataCardBike[i].quantity,
+    });
+  }
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: stripeItems,
+    mode: "payment",
+    success_url: "http://localhost:3000/confirm",
+    cancel_url: "http://localhost:3000/",
+  });
+
+  res.redirect(303, session.url);
+});
+
+router.get('/confirm', function (req, res, next) {
+  res.render('confirm')
 })
 
 module.exports = router;
